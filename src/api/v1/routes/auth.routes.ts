380836de05f87782795915590b008327e14083a7
Sarat Jabari <saratjabari905@gmail.com>
2025-10-23 15:24:00 +0300

    FC-49: Add /auth/login route

--- src/api/v1/routes/auth.routes.ts
+++ src/api/v1/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 import { validateSchema } from "../../middlewares/schemaValidator";
-import { registerSchema } from "../schemas/auth.schema";
+import { loginSchema, registerSchema } from "../schemas/auth.schema";
 
 const router = Router();
 
@@ -9,6 +9,11 @@ router.post(
   "/register",
   validateSchema(registerSchema, null, null),
   AuthController.register
+);
+router.post(
+  "/login",
+  validateSchema(loginSchema, null, null),
+  AuthController.login
 );
 
 export default router;

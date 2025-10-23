cebdfc6eb6f216c4a510bfcaec4d9abb74c7504f
Sarat Jabari <saratjabari905@gmail.com>
2025-10-23 15:23:48 +0300

    FC-49: Add AuthController login handler

--- src/api/v1/controllers/auth.controller.ts
+++ src/api/v1/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import type { Response } from "express";
-import type { RegisterRequest } from "../schemas/auth.schema";
+import type { RegisterRequest , LoginRequest } from "../requests/auth.request";
 import type {
   RegisterResponse,
   ErrorResponse,
@@ -7,6 +7,7 @@ import type {
 import { AuthService } from "../../../services/auth.service";
 import { CustomError } from "../../../utils/custom-error";
 import type { ValidatedRequest } from "../../middlewares/schemaValidator";
+import logger from "../../../configs/logger";
 
 export class AuthController {
   private static authService = new AuthService();
@@ -22,6 +23,39 @@ export class AuthController {
 
       res.status(201).json({
         data: user,
+      });
+    } catch (error) {
+      if (error instanceof CustomError) {
+        res.status(error.statusCode).json({
+          error: {
+            code: error.code ?? "ERROR",
+            message: error.message,
+          },
+        });
+        return;
+      }
+
+      res.status(500).json({
+        error: {
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An unexpected error occurred.",
+        },
+      });
+    }
+  }
+
+  static async login(req: ValidatedRequest, res: Response): Promise<void> {
+    const { username, password } = req.validatedBody as LoginRequest;
+    logger.info(`login attempt for user: ${username}`);
+
+    try {
+      const loginResult = await AuthController.authService.login(
+        // what is passed
+        { username, password }
+      );
+
+      res.status(200).json({
+        data: loginResult,
       });
     } catch (error) {
       if (error instanceof CustomError) {

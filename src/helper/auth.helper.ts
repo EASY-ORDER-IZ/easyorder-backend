3498afe33471850838a275fe642fe87a0098c409
Sarat Jabari <saratjabari905@gmail.com>
2025-11-02 07:43:35 +0200

    feat(helpers): add authentication helper for reusable auth logic

---
+++ src/helper/auth.helper.ts
@@ -0,0 +1,46 @@
+import { AppDataSource } from "../configs/database";
+import { User } from "../entities/User";
+import { LoginRequest } from "../api/v1/schemas/auth.schema";
+import { CustomError } from "../utils/custom-error";
+import { PasswordUtil } from "../utils/password.utils";
+import { AccountStatus, Role } from "../constants";
+
+export class AuthHelper {
+  private userRepository = AppDataSource.getRepository(User);
+
+  async getValidUser(data: LoginRequest): Promise<User | null> {
+    const user = await this.userRepository.findOne({
+      where: { email: data.email },
+      relations: ["userRoles"],
+    });
+
+    if (!user) {
+      throw new CustomError("Invalid email or password", 401, "AUTH_FAILED");
+    }
+
+    const passwordValid = PasswordUtil.verify(user.passwordHash, data.password);
+    if (!passwordValid) {
+      throw new CustomError("Invalid email or password", 401, "AUTH_FAILED");
+    }
+
+    if (user.emailVerified === null) {
+      throw new CustomError("Email not verified", 403, "EMAIL_NOT_VERIFIED");
+    }
+
+    if (user.accountStatus !== AccountStatus.ACTIVE) {
+      throw new CustomError("Account is not active", 403, "ACCOUNT_INACTIVE");
+    }
+    return user;
+  }
+
+  async getUserRole(user: User): Promise<Role> {
+    const roles = user.userRoles.map((role) => role.role);
+    let userRole: Role;
+    if (roles.includes(Role.ADMIN)) {
+      userRole = Role.ADMIN;
+    } else {
+      userRole = Role.CUSTOMER;
+    }
+    return userRole;
+  }
+}

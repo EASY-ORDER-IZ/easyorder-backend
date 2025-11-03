ba7cc4b4b012543db29910828a6437907f295c17
Sarat Jabari <saratjabari905@gmail.com>
2025-11-03 18:12:47 +0200

    feat(auth): add role-based authorization middleware

---
+++ src/api/middlewares/authorize.middleware.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from 'express';
+import logger from '../../configs/logger';
+
+
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    logger.debug(`Allowed roles: [${roles.join(', ')}]`);
+
+    const user = req.user;
+    if (!user || !roles.includes(user.role)) {
+      logger.warn('Access denied for user');
+      res.status(403).json({
+        message: 'Access denied. You are not authorized.',
+      });
+      return;
+    }
+    logger.info(`Access granted for user: ${user.userId} with role: ${user.role}`);
+    next();
+  };
+};
